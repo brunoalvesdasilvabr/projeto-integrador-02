b31c4e9a2363868e18dbc52470dcbe9a454a11a1
LuanaMaruya <34006195+LuanaMaruya@users.noreply.github.com>
2018-09-17 18:17:37 -0300

    modificações na home, footer

--- footer.php
+++ footer.php
@@ -1,63 +1,65 @@
+<!-- FOOTER -->
 <footer class="container">
-  <footer class="page-footer font-small cyan darken-3">
 
-      <!-- Footer Elements -->
-      <div class="container">
+<footer class="page-footer font-small cyan darken-3">
 
-        <!-- Grid row-->
-        <div class="row">
+    <!-- Footer Elements -->
+    <div class="container">
 
-          <!-- Grid column -->
-          <div class="col-md-12 py-5">
-            <div class="mb-5 flex-center">
+      <!-- Grid row-->
+      <div class="row">
 
-              <!-- Facebook -->
-              <a href="https://m.facebook.com/login/" title=""><img src="image/facebook.png" alt="Link para nossa página do Facebook." width="50" height="50" #id></a>
-                <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <!-- Grid column -->
+        <div class="col-md-12 py-5">
+          <div class="mb-5 flex-center">
 
-              </a>
-              <!-- Twitter -->
-              <a class="tw-ic">
-                  <a href="https://twitter.com/?lang=en" title="">  <img src="image/twitter.png" alt="Link para nossa página do Twitter." width="50" height="50" #id></a>
-                <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            <!-- Facebook -->
+            <a href="https://m.facebook.com/login/" title=""><img src="image/facebook.png" alt="Link para nossa página do Facebook." width="50" height="50" #id></a>
+              <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 
-              </a>
-              <!-- Google +-->
-              <a class="gplus-ic">
-                  <a href="https://myaccount.google.com/" title="">  <img src="image/gmail3.png" alt="Link para o Google+." width="50" height="50" #id></a>
-                <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+                <a href="https://twitter.com/?lang=en" title="">  <img src="image/twitter.png" alt="Link para nossa página do Twitter." width="50" height="50" #id></a>
+              <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 
-              </a>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+                <a href="https://myaccount.google.com/" title="">  <img src="image/gmail3.png" alt="Link para o Google+." width="50" height="50" #id></a>
+              <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 
-              <!--Instagram-->
-              <a class="ins-ic">
-                <a href="https://www.instagram.com/" title=""><img src="image/instagram2.png" alt="Link do Facebook." width="50" height="50" #id></a>
-                <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
 
-              </a>
-              <p class = "float-right"><a class = "login"rel="stylesheet" href="login.html">Login</a></p>
-              <p class = "float-right"><a class = "cadastro"rel="stylesheet" href="cadastro.html">Cadastro</a></p>
-              <p class = "float-right"> <a class = "faq" rel="stylesheet" href="faq.html">FAQ</a></p>
-              <p class = "float-right"><a class = "linktop" href="#">Topo</a></p>
-              <!-- <p class = "login"><a class = "linkloginhref="#"></a> </p> -->
+            <!--Instagram-->
+            <a class="ins-ic">
+              <a href="https://www.instagram.com/" title=""><img src="image/instagram2.png" alt="Link do Facebook." width="50" height="50" #id></a>
+              <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
 
-            </div>
+            </a>
+            <p class = "float-right"><a class = "login" rel="stylesheet" href="login.html">Login</a></p>
+            <p class = "float-right"><a class = "cadastro" rel="stylesheet" href="cadastro.html">Cadastro</a></p>
+            <p class = "float-right"> <a class = "faq" rel="stylesheet" href="faq.html">FAQ</a></p>
+            <p class = "float-right"><a class = "linktop" href="#">Topo</a></p>
+            <!-- <p class = "login"><a class = "linkloginhref="#"></a> </p> -->
+
           </div>
-          <!-- Grid column -->
-
         </div>
-        <!-- Grid row-->
+        <!-- Grid column -->
 
       </div>
-      <!-- Footer Elements -->
+      <!-- Grid row-->
 
-      <!-- Copyright -->
-      <div class="footer-copyright text-center py-3">© 2018 Copyright:
-        <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
-      </div>
-      <!-- Copyright -->
+    </div>
+    <!-- Footer Elements -->
 
-    </footer>
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <!-- Copyright -->
+    <!-- <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a> -->
+    </div>
+<footer class="container">
+<p class="float-right"><a href="#">Topo</a></p>
+<p>&copy; 2018 Skillancer. &middot; <a class= "privacidade" href="#">Privacidade</a> &middot; <a class= "termos" href="#">Termos</a></p>
 </footer>
+
+</main>
